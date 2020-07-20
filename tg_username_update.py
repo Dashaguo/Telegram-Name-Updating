387cb73387ecf699d6d6b438eaf5a34cff0073e8
Liwanqian <33348166+Liwanqian@users.noreply.github.com>
2020-07-20 16:00:08 +0800

    Update tg_username_update.py

--- tg_username_update.py
+++ tg_username_update.py
@@ -17,8 +17,8 @@ from telethon.tl.functions.account import UpdateProfileRequest
 from emoji import emojize
 
 
-dizzy = emojize(":dizzy:", use_aliases=True)
-cake = emojize(":cake:", use_aliases=True)
+bread = emojize(":bread:", use_aliases=True)
+beers = emojize(":beers:", use_aliases=True)
 all_time_emoji_name = ["clock12", "clock1230", "clock1", "clock130", "clock2", "clock230", "clock3", "clock330", "clock4", "clock430", "clock5", "clock530", "clock6", "clock630", "clock7", "clock730", "clock8", "clock830", "clock9", "clock930", "clock10", "clock1030", "clock11", "clock1130"]
 time_emoji_symb = [emojize(":%s:" %s, use_aliases=True) for s in all_time_emoji_name]
 
@@ -56,16 +56,16 @@ async def change_name_auto():
                 hsym = time_emoji_symb[(int(hour)%12)*2+shift]
                 # await client1.send_message('me', hsym)
                 for_fun = random.random() 
-                if for_fun < 0.10:
-                    last_name = '%s时%s分 %s' % (hour, minu, hsym)
+                if for_fun < 0.30:
+                    last_name = '%s:%s %s %s %s' % (hour, minu, p, abbwn, hsym)
                 elif for_fun < 0.30:
                     last_name = '%s:%s %s %s %s' % (hour, minu, p, abbwn, hsym)
-                elif for_fun < 0.60:
+                elif for_fun < 0.30:
                     last_name = '%s:%s %s UTC+8 %s' % (hour, minu, p, hsym)
-                elif for_fun < 0.90:
-                    last_name = '%s' % dizzy
+                elif for_fun < 0.30:
+                    last_name = '%s' % bread
                 else:
-                    last_name = '%s' % cake
+                    last_name = '%s' % beers
         
                 await client1(UpdateProfileRequest(last_name=last_name))
                 logger.info('Updated -> %s' % last_name)
